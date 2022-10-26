94058928dfb3ae5df9e51cdc2a2adb65fdc63175
NOURIGAT Adrien <adrien.nourigat@artfx34.lan>
2022-10-26 16:42:31 +0200

    [Add]

--- BitSet/BitSet.cpp
+++ BitSet/BitSet.cpp
@@ -2,7 +2,6 @@
 //
 
 #include <iostream>
-#include <iostream>
 #include <set>
 #include <bitset>
 #include <string>
@@ -10,6 +9,8 @@
 #include <algorithm>
 #include <functional>
 #include <string_view>
+#include <numeric>
+#include <limits>
 
 using std::vector;
 using namespace  std;
@@ -116,7 +117,7 @@ int DetectGap(std::string binaryValue)
 
 		}
 	}
-	return maxValue;
+return maxValue;
 }
 
 //Lesson 2
@@ -176,7 +177,7 @@ int FindOddInArray(vector<int>& A)
 
 //Lesson 3
 int JumpibgFrog(int X, int Y, int D) {
-	float result {0};
+	float result{ 0 };
 	if (X >= Y) return 0;
 
 	float reste = (Y - X) % (D);
@@ -199,11 +200,11 @@ int MissingElement(vector<int>& A)
 
 	if (A.at(0) != 1) return 1;
 
-	for (size_t i = 0; i < A.size() -1 ; i ++)
+	for (size_t i = 0; i < A.size() - 1; i++)
 	{
 		if (A.at(i + 1) != A.at(i) + 1)
 		{
-			return A.at(i)+1;
+			return A.at(i) + 1;
 		}
 	}
 	if (A.size() == 1) return A.back() + 1;
@@ -212,15 +213,58 @@ int MissingElement(vector<int>& A)
 
 int TapeEquilibrium(vector<int>& A)
 {
+	//But trouver quel est laplus petite différence
+	int jMax = 0;
+	int kMax = 0;
+//	std::set<int> minValue;
 
+	int minval = numeric_limits<float>::infinity();
+
+
+	for (size_t i = 1; i < A.size(); i++)
+	{
+		/*
+		//jMax = 0;
+		//kMax = 0;
+		
+
+		for (size_t j = 0; j < i; j++)
+		{
+			jMax += A.at(j);
+		}
+		for (size_t k = i; k < A.size(); k++)
+		{
+			kMax += A.at(k);
+
+		}*/
+		
+		jMax = accumulate(A.begin(), A.end()-i, 0);
+		kMax = accumulate(A.end() - i, A.end(), 0);
+		
+		int val = abs(jMax - kMax);
+
+		if (val == 0) return 0;
+
+		if (val < minval)
+		{
+			minval = val;
+		}
+		//minValue.insert(abs(jMax - kMax));
+
+		
+	}
+	//return *minValue.begin();
+	return minval;
 }
 
 int main()
 {
-	vector<int> vals{ 1,2 };
+	vector<int> vals{ 3,1,2,4,3 };
 	int x = 10;
 	int y = 85;
 	int d = 30;
+
+	cout << endl << TapeEquilibrium(vals) << endl;
 
 	//std::cout << DetectGap(IntToBin(145));
 
